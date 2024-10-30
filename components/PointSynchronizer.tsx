650acacba3f7d2a0b4cb31f4613eefd9a97a697b
cryptoaitm <cryptoaibtc@gmail.com>
2024-10-30 23:56:38 +0300

    Update PointSynchronizer.tsx

--- components/PointSynchronizer.tsx
+++ components/PointSynchronizer.tsx
@@ -73,7 +73,7 @@ export function PointSynchronizer() {
             if (unsynchronizedPoints > 1) {
                 syncWithServer();
             }
-        }, 3000);
+        }, 300);
 
         return () => {
             if (syncTimeoutRef.current) {
