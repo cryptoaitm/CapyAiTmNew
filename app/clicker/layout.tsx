aeab49daac74e8c62a98704e348f0a6337c28bfb
cryptoaitm <cryptoaibtc@gmail.com>
2024-11-01 23:29:58 +0300

    Update layout.tsx

--- app/clicker/layout.tsx
+++ app/clicker/layout.tsx
@@ -3,6 +3,17 @@
 import { WALLET_MANIFEST_URL } from '@/utils/consts';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
+import '@mysten/dapp-kit/dist/index.css';
+
+import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+const networks = {
+	devnet: { url: getFullnodeUrl('devnet') },
+	mainnet: { url: getFullnodeUrl('mainnet') },
+};
 export default function MyApp({
     children,
 }: Readonly<{
@@ -10,7 +21,13 @@ export default function MyApp({
 }>) {
     return (
         <TonConnectUIProvider manifestUrl={WALLET_MANIFEST_URL}>
+            <QueryClientProvider client={queryClient}>
+			<SuiClientProvider networks={networks} defaultNetwork="devnet">
+				<WalletProvider>
             {children}
+            </WalletProvider>
+			</SuiClientProvider>
+		</QueryClientProvider>
         </TonConnectUIProvider>
     );
 }
